50adfee7838ddcf0a28979fe5719c30868b9479f
Olena Ohranchuk <olena.ohranchuk@gmail.com>
2017-06-13 18:34:33 +0300

    fix wrong field name in addresses

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613144158) do
+ActiveRecord::Schema.define(version: 20170613153057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "addresses", force: :cascade do |t|
     t.string "country"
-    t.string "sity"
+    t.string "city"
     t.string "street"
     t.string "number"
     t.string "latitude"
