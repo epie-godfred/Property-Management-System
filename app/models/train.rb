0c7423b8188d5978475853096239c360efb099b9
Olena Ohranchuk <olena.ohranchuk@gmail.com>
2017-06-13 18:46:06 +0300

    train model test added

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,4 +1,7 @@
 class Train < ApplicationRecord
+  has_many :timetables
+  has_many :stations, through: :timetables
+
   validates :name, presence: true
   validates :number, presence: true, uniqueness: true
   validates :wagon_count, numericality: { greater_than_or_equal_to: 2, less_than_or_equal_to: 50 }
