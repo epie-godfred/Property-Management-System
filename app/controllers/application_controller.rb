9fab1ad37c04a731548a3fb823610d35b4ab5235
Olena Ohranchuk <olena.ohranchuk@gmail.com>
2017-06-14 12:02:49 +0300

    not_found_error caught

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound,         with: :render_not_found
+  rescue_from ActionController::RoutingError,       with: :render_not_found
+  rescue_from ::AbstractController::ActionNotFound, with: :render_not_found
+
+  def render_not_found(e)
+    render 'errors/error_404', status: 404
+  end
 end
